989320ddb84fb18a2cd3c4653dc8d293d549a3bb
ClaireYarber <clyarber@ou.edu>
2024-12-03 16:30:07 -0600

    Create header.php

---
+++ header.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?=$pageTitle?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/style.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="/">Navbar</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item"><a class="nav-link active" href="/">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/controllers/authors.php">Authors</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/controllers/books.php">Books</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/controllers/chapters.php">Chapters</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/controllers/pages.php">Pages</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
