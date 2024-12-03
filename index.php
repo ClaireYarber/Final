d3095d1a9985245984d34b0e14c98aa774b4a9e5
ClaireYarber <clyarber@ou.edu>
2024-12-03 16:40:12 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+// index.php
+<?php
+$pageTitle= "Home";
+include "views/header.php";
+?>
+<h1>Welcome to the Final Project</h1>
+<p>This is my Final project for MIS 4013.</p>
+<?php
+include "views/footer.php";
+?>
