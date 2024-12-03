2bf625853441546f6e89deaa3c8f28eed0cf8e87
ClaireYarber <clyarber@ou.edu>
2024-12-03 16:51:29 -0600

    Update view.header.php

--- view.header.php
+++ view.header.php
@@ -1,28 +1,35 @@
 <!doctype html>
 <html lang="en">
-<head>
+  <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?=$pageTitle?></title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="/css/style.css" rel="stylesheet"> <!-- Link to your custom CSS file -->
-</head>
-<body>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
     <div class="container">
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="/">Navbar</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item"><a class="nav-link active" href="/">Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="/controllers/authors.php">Authors</a></li>
-                        <li class="nav-item"><a class="nav-link" href="/controllers/books.php">Books</a></li>
-                        <li class="nav-item"><a class="nav-link" href="/controllers/chapters.php">Chapters</a></li>
-                        <li class="nav-item"><a class="nav-link" href="/controllers/pages.php">Pages</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="authors.php">Authors</a>
+        </li>
+     <li class="nav-item">
+          <a class="nav-link" href="books.php">Books</a>
+        </li><li class="nav-item">
+          <a class="nav-link" href="chapters.php">Chapters</a>
+           </li><li class="nav-item">
+          <a class="nav-link" href="pages.php">Pages</a>
+        </li> 
+      </ul>     
+    </div>
+  </div>
+</nav>
