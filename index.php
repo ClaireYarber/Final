9734a0cf07d8761ef5fc8f5445e5b526f6e74f20
ClaireYarber <clyarber@ou.edu>
2024-12-03 16:48:30 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
-// index.php
 <?php
 $pageTitle= "Home";
-include "views/header.php";
+Include "view-header.php";
 ?>
-<h1>Welcome to the Final Project</h1>
+<h1>Welcome to my Final Project</h1>
 <p>This is my Final project for MIS 4013.</p>
 <?php
-include "views/footer.php";
+Include "view-footer.php";
 ?>
