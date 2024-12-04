fb46864fa0c48a9fb2cc07564dc73408d3f32ae8
ClaireYarber <clyarber@ou.edu>
2024-12-04 11:11:28 -0600

    Update index.php

--- index.php
+++ index.php
@@ -7,3 +7,25 @@ include "view-header.php";
 <?php
 include "view-footer.php"; 
 ?>
+
+<form method="POST" action="index.php">
+    <label for="author_id">Select Author:</label>
+    <select name="author_id" id="author_id">
+        <option value="">-- Select an Author --</option>
+        <?php
+        // Fetch authors for the dropdown
+        $authorsQuery = "SELECT * FROM authors";
+        $authorsResult = mysqli_query($conn, $authorsQuery);
+
+        // Check if authors are fetched successfully
+        if ($authorsResult) {
+            while ($author = mysqli_fetch_assoc($authorsResult)) {
+                echo "<option value='" . $author['id'] . "'>" . $author['name'] . "</option>";
+            }
+        } else {
+            echo "<option value=''>No authors available</option>";
+        }
+        ?>
+    </select>
+    <button type="submit">Filter</button>
+</form>
